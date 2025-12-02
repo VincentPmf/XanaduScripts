53734791644b5eb68e099d13b0ebc805db306db9
c23014046 <vincent.causse@etu.univ-amu.fr>
2025-12-02 16:35:59 +0100

    test

--- Scripts/newUser.ps1
+++ Scripts/newUser.ps1
@@ -30,7 +30,7 @@ function New-User {
 
     $DomainDN = (Get-ADDomain).DistinguishedName
     $SearchBase = "OU=Groups,$DomainDN"
-    Write-Host "$SearchBase"
+    Write-Host "Test $SearchBase"
 
     Write-Host "`n=== Groupes AD disponibles ===" -ForegroundColor Cyan
     Get-ADGroup -Filter * -SearchBase $SearchBase  |
